baecb0d3dd6d8bdc21d935c868013c1265fb91de
MiguelUbeda <miguelubeda09@hotmail.com>
2022-08-05 18:56:41 +0000

    Home/Navbar/Footer

--- src/front/js/component/footer.js
+++ src/front/js/component/footer.js
@@ -1,10 +1,61 @@
 import React, { Component } from "react";
 
 export const Footer = () => (
-	<footer className="footer mt-auto py-3 text-center">
-		<p>
-			Made with <i className="fa fa-heart text-danger" /> by{" "}
-			<a href="http://www.4geeksacademy.com">4Geeks Academy</a>
-		</p>
-	</footer>
+<div className="container-fluid p-0">
+
+  <footer className="bg-light text-center text-white">
+   
+  <div className="container p-4">
+    <section className="">
+      <a
+        className="btn btn-primary btn-floating m-1"
+        style={{backgroundColor: "#3b5998"}}
+        href="#!"
+        role="button"
+        ><i className="fab fa-facebook-f"></i
+      ></a>
+      <a
+        className="btn btn-primary btn-floating m-1"
+    	style= {{backgroundColor: "#55acee"}}
+        href="#!"
+        role="button"
+        ><i className="fab fa-twitter"></i
+      ></a>
+      <a
+        className="btn btn-primary btn-floating m-1"
+        style= {{backgroundColor: "#dd4b39"}}
+        href="#!"
+        role="button"
+        ><i className="fab fa-google"></i
+      ></a>
+      <a
+        className="btn btn-primary btn-floating m-1"
+        style= {{backgroundColor: "ac2bac"}}
+        href="#!"
+        role="button"
+        ><i className="fab fa-instagram"></i
+      ></a>
+      <a
+        className="btn btn-primary btn-floating m-1"
+        style= {{backgroundColor: "#0082ca"}}
+        href="#!"
+        role="button"
+        ><i className="fab fa-linkedin-in"></i
+      ></a>
+      <a
+        className="btn btn-primary btn-floating m-1"
+        style= {{backgroundColor: "#333333"}}
+        href="#!"
+        role="button"
+        ><i className="fab fa-github"></i
+      ></a>
+    </section>
+  </div>
+  <div className="text-center p-3" style= {{backgroundColor: "rgba(0, 0, 0, 0.2)"}}>
+    © 2022 Teamder
+    <a className="text-white" href="https://mdbootstrap.com/">Teamder.es</a>
+  </div>
+</footer>
+  
+</div>
 );
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,6 +1,6 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import rigoImageUrl from "../../img/rigo-baby.jpg";
+import teamderImg from "/workspace/Teamder/src/front/img/teamderImg.png";
 import "../../styles/home.css";
 
 export const Home = () => {
@@ -10,7 +10,7 @@ export const Home = () => {
 		<div className="text-center mt-5">
 			<h1>Hello Rigo!!</h1>
 			<p>
-				<img src={rigoImageUrl} />
+				<img src={teamderImg} className="w-50 h-50"/>
 			</p>
 			<div className="alert alert-info">
 				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
