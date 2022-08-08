044ab3205a4831d47db403cc433eabed3feed61e
siemeris <siemeris@gmail.com>
2022-08-08 20:54:50 +0000

    navbar responsive

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -3,23 +3,17 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
   return (
-    <nav className="navbar navbar-light bg-light">
-      <div className="container">
-        <Link to="/">
-          <span className="navbar-brand mb-0 h1">React Boilerplate</span>
-        </Link>
-        <div className="ml-auto">
-          <Link to="/signup">
-            <button className="btn btn-primary">
-              Sign Up
-            </button>
-          </Link>
-          <Link to="/login">
-            <button className="btn btn-primary ms-3">
-              Login
-            </button>
-          </Link>
-        </div>
+<nav class="navbar navbar-expand-lg navbar-light bg-light ">
+        <Link to="/" class="navbar-brand ps-2">Teamder</Link>
+        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon"></span>
+        </button>
+      <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
+          <ul class="navbar-nav ms-auto pe-5">
+
+              <li class="nav-item"><Link to="/signup" class="nav-link"><button className="btn btn-primary">Sign Up</button></Link></li>
+              <li class="nav-item"><Link to="/login" class="nav-link"><button className="btn btn-primary ms-3">Login</button></Link></li>
+         </ul>
       </div>
     </nav>
   );
