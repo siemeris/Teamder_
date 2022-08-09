4a242164e1235688ee0d8ef85a65e83906ec94cc
siemeris <siemeris@gmail.com>
2022-08-09 07:02:25 +0000

    responsive and className

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -3,16 +3,16 @@ import { Link } from "react-router-dom";
 
 export const Navbar = () => {
   return (
-<nav class="navbar navbar-expand-lg navbar-light bg-light ">
-        <Link to="/" class="navbar-brand ps-2">Teamder</Link>
-        <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
-            <span class="navbar-toggler-icon"></span>
+<nav className="navbar navbar-expand-lg navbar-light bg-light ">
+        <Link to="/" className="navbar-brand ps-2">Teamder</Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+            <span className="navbar-toggler-icon"></span>
         </button>
-      <div class="collapse navbar-collapse" id="navbarTogglerDemo01">
-          <ul class="navbar-nav ms-auto pe-5">
+      <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
+          <ul className="navbar-nav ms-auto pe-5">
 
-              <li class="nav-item"><Link to="/signup" class="nav-link"><button className="btn btn-primary">Sign Up</button></Link></li>
-              <li class="nav-item"><Link to="/login" class="nav-link"><button className="btn btn-primary ms-3">Login</button></Link></li>
+              <li className="nav-item"><Link to="/signup" className="nav-link"><button className="btn btn-primary">Sign Up</button></Link></li>
+              <li className="nav-item"><Link to="/login" className="nav-link"><button className="btn btn-primary ms-3">Login</button></Link></li>
          </ul>
       </div>
     </nav>
