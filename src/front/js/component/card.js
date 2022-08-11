b3f518449611d7e531fc24d6e96226bf23086503
IsaGalo80 <isagalo1980@outlook.com>
2022-08-11 17:01:44 +0000

    falta hacer boton para enlazar componentes

--- src/front/js/component/card.js
+++ src/front/js/component/card.js
@@ -5,80 +5,97 @@ import React, { useContext } from "react";
 export const Card = () => {
   return (
     <>
-      <div className="container mx-auto mt-4">
-        <div className="row">
-          <div className="col-md-4">
-            <div
-              className="card"
-              style={{
-                width: "20rem",
-                height: "20rem",
-              }}
-            >
-              {/* esto sería para que al pinchar la imagen nos redirija a otro componente */}
-              <a href="http://www.secretariaplus.com">
-                {" "}
-                <img
-                  src="https://images.pexels.com/photos/5235781/pexels-photo-5235781.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
-                  className="card-img-top"
-                  alt="grupo de personas jugando futbol"
-                />{" "}
-              </a>
-              {/* esta sería la imagen sola */}
-              {/* <img
-                src="https://us.123rf.com/450wm/rrraven/rrraven1310/rrraven131000105/23083801-vector-icono-de-f%C3%BAtbol-azul-sobre-fondo-blanco-.jpg?ver=6"
-                className="card-img-top"
-                alt="balon de futbol"
-              /> */}
-              <div className="card-body">
-                <h5 className="card-title">Futbol</h5>
+      <div className="row">
+        <div className="col-4">
+          <div className="container mx-auto mt-4">
+            <div className="row">
+              <div className="col-md-4">
+                <div
+                  className="card"
+                  style={{
+                    width: "20rem",
+                    height: "20rem",
+                  }}
+                >
+                  {/* esto sería para que al pinchar la imagen nos redirija a otro componente */}
+                  <a href="http://www.secretariaplus.com">
+                    {" "}
+                    <img
+                      src="https://images.pexels.com/photos/5235781/pexels-photo-5235781.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                      className="card-img-top"
+                      alt="grupo de personas jugando futbol"
+                    />{" "}
+                  </a>
+                  <div className="card-body">
+                    <h5 className="card-title">Futbol</h5>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
 
-                {/* enlaces para redirigir si es que no se quiere que la imagen funcione como boton */}
-                {/* <a href="#" className="btn mr-2">
-                  <i className="fas fa-link"></i> Activity description
-                </a> */}
-                {/* <a href="#" className="btn ">
-                  <i class="fa-duotone fa- fa sun-cloud"></i>clima
-                </a> */}
+        {/* Segunda card */}
+        <div className="col-4">
+          <div className="container mx-auto mt-4">
+            <div className="row">
+              <div className="col-md-4">
+                <div
+                  className="card"
+                  style={{
+                    width: "20rem",
+                    height: "20rem",
+                  }}
+                >
+                  {/* esto sería para que al pinchar la imagen nos redirija a otro componente */}
+                  <a href="http://www.secretariaplus.com">
+                    {" "}
+                    <img
+                      src="https://images.pexels.com/photos/10855503/pexels-photo-10855503.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2"
+                      className="card-img-top"
+                      alt="grupo de personas jugando voley"
+                    />{" "}
+                  </a>
+                  <div className="card-body">
+                    <h5 className="card-title">Voley</h5>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        {/* Tercera card */}
+
+        <div className="col-4">
+          <div className="container mx-auto mt-4">
+            <div className="row">
+              <div className="col-md-4">
+                <div
+                  className="card"
+                  style={{
+                    width: "20rem",
+                    height: "20rem",
+                  }}
+                >
+                  {/* esto sería para que al pinchar la imagen nos redirija a otro componente */}
+                  <a href="http://www.secretariaplus.com">
+                    {" "}
+                    <img
+                      src="https://images.pexels.com/photos/5560205/pexels-photo-5560205.jpeg?auto=compress&cs=tinysrgb&w=800"
+                      className="card-img-top"
+                      alt="grupo de personas jugando basquet"
+                    />{" "}
+                  </a>
+                  <div className="card-body">
+                    <h5 className="card-title">Basquet</h5>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
         </div>
       </div>
-      {/* ESTA SERÍA LA DEL DETALLE
-      <div className="container mx-auto mt-4">
-        <div className="row">
-          <div className="col-md-4">
-            <div
-              className="card"
-              style={{
-                width: "20rem",
-                height: "30rem",
-              }}
-            >
-              <img
-                src="https://us.123rf.com/450wm/rrraven/rrraven1310/rrraven131000105/23083801-vector-icono-de-f%C3%BAtbol-azul-sobre-fondo-blanco-.jpg?ver=6"
-                className="card-img-top"
-                alt="balon de futbol"
-              />
-              <div className="card-body">
-                <h5 className="card-title">Futbol</h5>
-                <h6 className="card-subtitle mb-2 text-muted">Date</h6>
-                <h6 className="card-subtitle mb-2 text-muted">City</h6>
-                <h6 className="card-subtitle mb-2 text-muted">
-                  <i className="fa-solid fa fa-user"></i>
-                </h6>
-                <a href="#" className="btn mr-2">
-                  <i class="fa-solid fa-calendar-days"></i>Apuntarse
-                </a>
-                <a href="#" className="btn ">
-                  <i class="fa-solid fa fa-sun"></i>clima
-                </a>
-              </div>
-            </div>
-          </div>
-        </div>
-      </div> */}
     </>
   );
 };
