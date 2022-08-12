43246ce560797d87b431911237ee961dda92381e
IsaGalo80 <isagalo1980@outlook.com>
2022-08-12 19:38:38 +0000

    hecho

--- src/front/js/component/card.js
+++ src/front/js/component/card.js
@@ -1,7 +1,6 @@
 import React, { useContext } from "react";
 import { Link } from "react-router-dom";
-// import PropTypes from "prop-types";
-// import { Context } from "../store/appContext";
+
 export const Card = () => {
   return (
     <>
