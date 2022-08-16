c1acd95139463e2f00a0d2544f38f2c237c000f6
TomasCarranzaC <tomascarranzac@gmail.com>
2022-08-16 09:44:15 +0000

    mods navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,26 +1,35 @@
 import React from "react";
+import "../../styles/navbar.css";
 import { Link } from "react-router-dom";
+import Logo2 from "/workspace/Teamder/src/front/img/Logo2.png"
 
 export const Navbar = () => {
   return (
     <nav className="navbar navbar-light bg-light">
-      <div className="container">
-        <Link to="/">
-          <span className="navbar-brand mb-0 h1">React Boilerplate</span>
-        </Link>
-        <div className="ml-auto">
-          <Link to="/signup">
-            <button className="btn btn-primary">
-              Sign Up
-            </button>
-          </Link>
-          <Link to="/login">
-            <button className="btn btn-primary ms-3">
-              Login
-            </button>
-          </Link>
-        </div>
-      </div>
-    </nav>
+  
+    <a className="TeamderLogo navbar-brand ml-auto d-flex ms-3" href="#">
+      <img src= {Logo2} alt="logo" width="30" height="30" className=" d-inline-block align-text-top"/>
+      <span className="ms-2">Teamder</span>
+    </a>
+    <ul className="list-unstyled">
+    <li class="nav-item dropdown me-3">
+          <a class="nav-link dropdown-toggle text-info " href="#" id="navbarDropdownMenuLink" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+          <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" fill="currentColor" class="bi bi-person-circle text-info" viewBox="0 0 16 16">
+  <path d="M11 6a3 3 0 1 1-6 0 3 3 0 0 1 6 0z"/>
+  <path fill-rule="evenodd" d="M0 8a8 8 0 1 1 16 0A8 8 0 0 1 0 8zm8-7a7 7 0 0 0-5.468 11.37C3.242 11.226 4.805 10 8 10s4.757 1.225 5.468 2.37A7 7 0 0 0 8 1z"/>
+</svg>
+          </a>
+          <ul class="dropdown-menu dropdown-menu-end" aria-labelledby="navbarDropdownMenuLink">
+            <li><a class="dropdown-item" href="#">Login</a></li>
+            <li><a class="dropdown-item" href="#">Signup</a></li>
+            <li><a class="dropdown-item" href="#">Logout</a></li>
+          </ul>
+        </li>
+        </ul>
+
+    
+   
+          
+</nav>
   );
 };
--- src/front/js/pages/home.js
+++ src/front/js/pages/home.js
@@ -1,25 +1,15 @@
 import React, { useContext } from "react";
 import { Context } from "../store/appContext";
-import teamderImg from "/workspace/Teamder/src/front/img/teamderImg.png";
 import "../../styles/home.css";
+import Jumbotron02 from "/workspace/Teamder/src/front/img/Jumbotron02.png"
 
 export const Home = () => {
 	const { store, actions } = useContext(Context);
 
 	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
+		<div className="text-center mt-1">
 			<p>
-				<img src={teamderImg} className="w-50 h-50"/>
-			</p>
-			<div className="alert alert-info">
-				{store.message || "Loading message from the backend (make sure your python backend is running)..."}
-			</div>
-			<p>
-				This boilerplate comes with lots of documentation:{" "}
-				<a href="https://github.com/4GeeksAcademy/react-flask-hello/tree/95e0540bd1422249c3004f149825285118594325/docs">
-					Read documentation
-				</a>
+				<img src={Jumbotron02} className="w-75 h-25"/>
 			</p>
 		</div>
 	);
