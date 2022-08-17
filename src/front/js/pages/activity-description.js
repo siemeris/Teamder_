dfee97c3d1f617c7e68814ffd7609400272bda1a
MiguelUbeda <miguelubeda09@hotmail.com>
2022-08-17 08:55:00 +0000

    First sketch, ready to merge

--- src/front/js/pages/activity-description.js
+++ src/front/js/pages/activity-description.js
@@ -17,15 +17,15 @@ export const ActivityDescription = () => {
             <h1 className="d-block text-center text-info m-5">Título de la actividad</h1>
             <div className="d-flex justify-content-center">
             <img src="https://upload.wikimedia.org/wikipedia/commons/thumb/b/b9/Football_iu_1996.jpg/1024px-Football_iu_1996.jpg" className="width: w-25 h-25" alt="Nope"/>
-                <div className="card ms-5">
-                    <div className="card-header bg-info">
+                <div className="card ms-5 shadow">
+                    <div className="card-header bg-info text-center">
                         Description
                     </div>
                     <ul className="list-group list-group-flush">
                         <li className="list-group-item">Participantes</li>
                         <li className="list-group-item">Localización</li>
                         <li className="list-group-item">Descripción usuario</li>
-                        <li className="list-group-item text-center"><button type="button" class="btn btn-info">Apuntarse</button></li>
+                        <li className="list-group-item text-center"><button type="button" class="btn btn-info text-white">Apuntarse</button></li>
                     </ul>
                 </div>
             </div>
