bb3268909b69ed160ecf84fef796cd52807e1765
siemeris <siemeris@gmail.com>
2022-08-17 18:37:19 +0000

    tabla de usuarios y actividades

--- src/api/models.py
+++ src/api/models.py
@@ -10,10 +10,10 @@ class User(db.Model):
     gender= db.Column(db.String(120), unique=True, nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
     password = db.Column(db.String(80), unique=False, nullable=False)
-    mobile= db.Column(db.Integer(120), unique=True, nullable=False)
+    mobile= db.Column(db.Integer, unique=True, nullable=False)
     address = db.Column(db.String(80), unique=False, nullable=False)
-    is_active = db.Column(db.Boolean(), unique=False, nullable=False)
-    actividades = db.relationship('Actividades', backref='user', lazy=True)
+    is_active = db.Column(db.Boolean, unique=False, nullable=False)
+    activities = db.relationship('Activities', backref='user', lazy=True)
     def __repr__(self):
          return '<User {self.email}>'
     def serialize(self):
@@ -27,20 +27,20 @@ class User(db.Model):
              "password": self.password,
              "mobile": self.mobile,
              "address": self.address,
-             "actividades": obtain_actividades()
+            #  "activities": obtain_actividades()
          }
 class Activities(db.Model):
     id = db.Column(db.Integer, primary_key=True)
     name= db.Column(db.String(120), unique=True, nullable=False)
     category=db.Column(db.String(120), unique=True, nullable=False)
-    date= db.Column(db.Integer(120), unique=True, nullable=False)
-    time=db.Column(db.Integer(120), unique=True, nullable=False)
+    date= db.Column(db.Integer, unique=True, nullable=False)
+    time=db.Column(db.Integer, unique=True, nullable=False)
     city = db.Column(db.String(120), unique=True, nullable=False)
     location = db.Column(db.String(120), unique=True, nullable=False)
     weather = db.Column(db.String(120), unique=True, nullable=False)
-    players = db.Column(db.Integer(120), unique=True, nullable=False)
-    latitude = db.Column(db.Integer(120), unique=True, nullable=False)
-    longitude = db.Column(db.Integer(120), unique=True, nullable=False)
+    players = db.Column(db.Integer, unique=True, nullable=False)
+    latitude = db.Column(db.Integer, unique=True, nullable=False)
+    longitude = db.Column(db.Integer, unique=True, nullable=False)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'),
         nullable=False)
     def __repr__(self):
