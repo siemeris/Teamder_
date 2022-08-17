7db6653a210361394fbe5aab7d54e2117903de73
TomasCarranzaC <tomascarranzac@gmail.com>
2022-08-17 15:47:18 +0000

    navbar actualizada

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -1,6 +1,7 @@
 import React from "react";
 import { Link } from "react-router-dom";
 import Logo2 from "/workspace/Teamder/src/front/img/Logo2.png";
+import "/workspace/Teamder/src/front/styles/navbar.css"
 
 
 export const Navbar = () => {
@@ -15,23 +16,23 @@ export const Navbar = () => {
           height="30"
           className=" d-inline-block align-text-top"
         />
-        <span className="textologo ms-2 "><Link to="/" className="navbar-brand ps-2">Teamder</Link></span>
+        <span className="ms-2 "><Link to="/" className="textologo navbar-brand ps-2"><span className="">Teamder</span></Link></span>
       </a>
-        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
+        <button className="navbar-toggler text-info" type="button" data-bs-toggle="collapse" data-bs-target="#navbarTogglerDemo01" aria-controls="navbarTogglerDemo01" aria-expanded="false" aria-label="Toggle navigation">
             <span className="navbar-toggler-icon"></span>
         </button>
       <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
           <ul className="navbar-nav ms-auto pe-5">
               <li className="nav-item">
                 <Link to="/signup" className="nav-link">
-                  <button class="btn btn-secondary me-3">
-                  <i class="fas fa-calendar"></i>
+                  <button class="btn btn-light me-3 btn-outline-info">
+                  <i class="fas fa-calendar-check text-info"></i>
                   </button>
                 </Link></li>
               <li className="nav-item">
               <div class="dropdown mt-2 ">
-                <button class="btn btn-secondary dropdown-toggle" type="button" data-bs-toggle="dropdown" aria-expanded="false">
-                  <i class="fas fa-user"></i>
+                <button class="btn btn-light dropdown-toggle text-info btn-outline-info" type="button" data-bs-toggle="dropdown" aria-expanded="false">
+                  <i class="fas fa-user text-info"></i>
                 </button>
                 <ul class="dropdown-menu dropdown-menu-end">
                   <li><a class="dropdown-item" href="#">Sign up</a></li>
