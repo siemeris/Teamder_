71902e9232c940d62e64f55bb484345f99642fb2
TomasCarranzaC <tomascarranzac@gmail.com>
2022-08-18 09:34:24 +0000

    iconos navbar

--- src/front/js/component/navbar.js
+++ src/front/js/component/navbar.js
@@ -25,7 +25,7 @@ export const Navbar = () => {
             <span className="navbar-toggler-icon"></span>
         </button>
       <div className="collapse navbar-collapse" id="navbarTogglerDemo01">
-          <ul className="navbar-nav ms-auto pe-5">
+          <ul className="navbar-nav ms-auto pe-5 navbar-item-end">
               <li className="nav-item">
                 <Link to="/activitypanel" className="nav-link">
                   <button className="btn btn-light me-3 btn-outline-info">
@@ -38,9 +38,18 @@ export const Navbar = () => {
                   <i className="fas fa-user text-info"></i>
                 </button>
                 <ul className="dropdown-menu dropdown-menu-end">
-                  <li><a className="dropdown-item" data-bs-toggle="modal" data-bs-target="#staticBackdrop">Sign Up</a></li>
-                  <li><a className="dropdown-item" data-bs-toggle="modal" data-bs-target="#staticBackdropLOG">Login</a></li>
-                  <li><a className="dropdown-item" href="/logout">Logout</a></li>
+                  <li><a className="dropdown-item" data-bs-toggle="modal" data-bs-target="#staticBackdrop"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-person-plus-fill text-info" viewBox="0 0 16 16">
+  <path d="M1 14s-1 0-1-1 1-4 6-4 6 3 6 4-1 1-1 1H1zm5-6a3 3 0 1 0 0-6 3 3 0 0 0 0 6z"/>
+  <path fill-rule="evenodd" d="M13.5 5a.5.5 0 0 1 .5.5V7h1.5a.5.5 0 0 1 0 1H14v1.5a.5.5 0 0 1-1 0V8h-1.5a.5.5 0 0 1 0-1H13V5.5a.5.5 0 0 1 .5-.5z"/>
+</svg>Sign Up</a></li>
+                  <li><a className="dropdown-item" data-bs-toggle="modal" data-bs-target="#staticBackdropLOG"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-in-right text-info" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M6 3.5a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v9a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-2a.5.5 0 0 0-1 0v2A1.5 1.5 0 0 0 6.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-9A1.5 1.5 0 0 0 14.5 2h-8A1.5 1.5 0 0 0 5 3.5v2a.5.5 0 0 0 1 0v-2z"/>
+  <path fill-rule="evenodd" d="M11.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 1 0-.708.708L10.293 7.5H1.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
+</svg>Login</a></li>
+                  <li><a className="dropdown-item" href="/logout"><svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" fill="currentColor" class="bi bi-box-arrow-right text-info" viewBox="0 0 16 16">
+  <path fill-rule="evenodd" d="M10 12.5a.5.5 0 0 1-.5.5h-8a.5.5 0 0 1-.5-.5v-9a.5.5 0 0 1 .5-.5h8a.5.5 0 0 1 .5.5v2a.5.5 0 0 0 1 0v-2A1.5 1.5 0 0 0 9.5 2h-8A1.5 1.5 0 0 0 0 3.5v9A1.5 1.5 0 0 0 1.5 14h8a1.5 1.5 0 0 0 1.5-1.5v-2a.5.5 0 0 0-1 0v2z"/>
+  <path fill-rule="evenodd" d="M15.854 8.354a.5.5 0 0 0 0-.708l-3-3a.5.5 0 0 0-.708.708L14.293 7.5H5.5a.5.5 0 0 0 0 1h8.793l-2.147 2.146a.5.5 0 0 0 .708.708l3-3z"/>
+</svg>Logout</a></li>
                 </ul>
               </div>
               </li>
