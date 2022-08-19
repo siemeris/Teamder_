5fe26f08659f1e3685272279d4168259bd8a0525
siemeris <siemeris@gmail.com>
2022-08-19 19:34:30 +0000

    con endpoints de signup login y privated

--- src/api/routes.py
+++ src/api/routes.py
@@ -31,5 +31,38 @@ def obtenerActivity():
     response_body = "OK"
     return jsonify(response_body), 200
 
+@api.route('/signup', methods=['POST'])
+def signup():
+    request_body = request.get_json(force=True)
+    user = User( name = request_body["name"], username = request_body["username"], lastname = request_body["lastname"], age = request_body["age"], gender = request_body["gender"],email = request_body["email"], password = request_body["password"])
+    db.session.add(user)
+    db.session.commit()
+    # my_token = create_access_token(identity = user.id)
+    return jsonify(), 200
 
+
+@api.route("/token", methods=["POST"])
+def create_token():
+    email = request.json.get("email", None)
+    password = request.json.get("password", None)
+    # Consulta la base de datos por el nombre de usuario y la contraseña
+    user = User.query.filter_by(email=email, password=password).first()
+    if user is None:
+    # el usuario no se encontró en la base de datos
+        return jsonify({"msg": "Bad username or password"}), 401
+    
+    # crea un nuevo token con el id de usuario dentro
+    access_token = create_access_token(identity=user.id)
+    return jsonify({ "token": access_token, "user_id": user.id })
+
+# Protege una ruta con jwt_required, bloquea las peticiones
+# sin un JWT válido presente.
+@api.route("/privated", methods=["GET"])
+@jwt_required()
+def protected():
+    # Accede a la identidad del usuario actual con get_jwt_identity
+    current_user_id = get_jwt_identity()
+    user = User.query.get(current_user_id)
+    
+    return jsonify({"id": user.id, "email": user.email }), 200
 
