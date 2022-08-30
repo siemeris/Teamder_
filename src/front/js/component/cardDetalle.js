c012c32243c23351621d13e609c581e1e8fff933
siemeris <siemeris@gmail.com>
2022-08-30 18:46:28 +0000

    falta previsión del día de la actividad y se puede mejorar ubicación por lat y long

--- src/front/js/component/cardDetalle.js
+++ src/front/js/component/cardDetalle.js
@@ -17,65 +17,70 @@ export const CardDetalle = () => {
   })
   const [location, setLocation] = useState("")
 
- 
-  // const url2 = "https://api.openweathermap.org/data/2.5/forecast?q=" + location + "&appid=74b3467d2c3033271c21502ee8e7ca5e&units=metric"
 
-  useEffect(()=>{actions.getActivities();},[])
-  
-useEffect(()=>{
-   actions.getWeather()
- },[store.location])
-  
+  // const url2 = "https://api.openweathermap.org/data/2.5/forecast/daily?q=" + location + "&appid=74b3467d2c3033271c21502ee8e7ca5e&units=metric"
+
+  useEffect(() => { actions.getActivities(); }, [])
+
+  // useEffect(()=>{
+  //    actions.getWeather()
+  //  },[store.locationList])
+
+  console.log(store.locationList, "listado location")
+  console.log(store.tempList, "listado temperaturas")
+  console.log(store.iconsList, "listado URLiconos")
 
   return (
     <div className="container mt-4">
-      <div className="row d-flex flex-column" style={{height: "16rem", overflowX:"auto"}}>
-      {store.activities.map((value,index) => {
-        store.location=value.city
-        console.log(store.location, "store.location")
-        console.log(value.city)
-        // actions.getWeather()
-        return (
-          
-          <div key={index} className="card px-1 mx-2 py-1 border border-light shadow border-4 rounded-1" style={{height: "14rem", width: '14rem'}}>
-            <div className="card-body">
-              <h5 className="card-title text-center mt-1">
-                {" "}
-                <Link to="/activity-description">
-                  <img
-                    src={soccer}
-                    className="card-img-top mx-2"
-                    style={{ width: "1.5rem" }}
-                    alt="group of people playing soccer"
-                  />
-                </Link>
-                {value.category}
-              </h5>
-              <h6 className="card-subtitle mb-2 text-muted ">{value.date}</h6>
-              <h6 className="card-subtitle mb-2 text-muted">{value.location}, {value.city}</h6>
-                  <span>
-                    
-                    <img src={store.urlIcons[index]}></img>
-                    {temp}ºC
-                  </span>
-              <hr></hr>
-              <div className="text-center mt-2">
-                <button
-                  type="button"
-                  className="btn btn-outline-info "
-                  id="boton usuario"
-                  onClick={() => {actions.joinActivity({
-                    index: value.id
-                  });
-                  }}
-                >
-                  Join
-                </button>
+      <div className="row d-flex flex-column" style={{ height: "18rem", overflowX: "auto" }}>
+        {store.activities.map((value, index) => {
+          // store.location=value.city
+          // console.log(store.location, "store.location")
+          console.log(value.city)
+          // actions.getWeather(value.city)
+          return (
+
+            <div key={index} className="card px-1 mx-2 py-1 border border-light shadow border-4 rounded-1" style={{ height: "16rem", width: '14rem' }}>
+              <div className="card-body">
+                <h5 className="card-title text-center mt-1">
+                  {" "}
+                  <Link to="/activity-description">
+                    <img
+                      src={soccer}
+                      className="card-img-top mx-2"
+                      style={{ width: "1.5rem" }}
+                      alt="group of people playing soccer"
+                    />
+                  </Link>
+                  {value.category}
+                </h5>
+                <h6 className="card-subtitle mb-2 text-muted ">{value.date}</h6>
+                <h6 className="card-subtitle mb-2 text-muted">{value.location}</h6>
+                <h6 className="card-subtitle mb-2 text-muted">{value.city}</h6>
+                <div className="text-center">
+                  <img src={store.iconsList[index]}></img>
+                  {store.tempList[index]}ºC
+                </div>
+                <hr className="m-2"></hr>
+                <div className="text-center">
+                  <button
+                    type="button"
+                    className="btn btn-outline-info mt-1"
+                    id="boton usuario"
+                    onClick={() => {
+                      actions.joinActivity({
+                        index: value.id
+                      });
+                    }}
+                  >
+                    Join
+                  </button>
+                </div>
               </div>
             </div>
-          </div>
-        )})}
-        </div>
-        </div>
-        )
+          )
+        })}
+      </div>
+    </div>
+  )
 };
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -19,109 +19,60 @@ const getState = ({ getStore, getActions, setStore }) => {
 			// 	// 	initial: "white"
 			// 	// }
 			// ]
-			activities: [],
 			markers: [],
 			dates: [],
-			urlIcons:[],
-			location: "",
+			iconCode:"",
+			urlIcon:"",
+			iconsList: [],
+			locationList: [],
+			tempList: []
 
 		},
 		actions: {
-			getWeather: ()=> {
-				let { location } = getStore()
-				console.log(location, "location")
-				const url = `https://api.openweathermap.org/data/2.5/weather?q=` + location + `&lang=en&units=metric&appid=74b3467d2c3033271c21502ee8e7ca5e`
-				  fetch(url)
-				  .then((resp) => {
-					if (resp.ok) {
-					  console.log("El request se hizo bien");
-					  return resp.json();
-					} else {
-					  console.log("Hubo un Error " + resp.status + " en el request");
-					}
-				  })
-				  .then((data) => {
-					//here is were your code should start after the fetch finishes
-					console.log("Este es el body del request", data); //this will print on the console the exact object received from the server
-					// console.log(body.map((t) => t.label));
-					// setLista(body.map((t) => t.label));
-					console.log(data.main.temp)
-					setTemp(Math.round(data.main.temp))
-			
-					// icono de la API estático
-					let iconCode = data.weather[0].icon
-					const urlIcon = `http://openweathermap.org/img/wn/${iconCode}.png`
-					let { urlIcons } = getStore()
-					urlIcons.push(urlIcon)
-					
+			// getWeather: () => {
+			// 	let { locationList } = getStore()
+			// 	console.log(locationList, "location get weather")
+			// 	locationList.map((value,index)=>{
+			// 		const url = "https://api.openweathermap.org/data/2.5/weather?q=" + value + "&lang=en&units=metric&appid=74b3467d2c3033271c21502ee8e7ca5e"
+			// 	console.log(url, "url")
+			// 		fetch(url)
+			// 		.then((resp) => {
+			// 			if (resp.ok) {
+			// 				console.log("El request se hizo bien");
+			// 				return resp.json();
+			// 			} else {
+			// 				console.log("Hubo un Error " + resp.status + " en el request");
+			// 			}
+			// 		})
+			// 		.then((data) => {
+			// 			//here is were your code should start after the fetch finishes
+			// 			console.log("Este es el body del request", data); //this will print on the console the exact object received from the server
+			// 			// console.log(body.map((t) => t.label));
+			// 			// setLista(body.map((t) => t.label));
+			// 			console.log(data.main.temp)
+			// 			// setTemp(Math.round(data.main.temp))
+			// 			let { tempList } = getStore()
+			// 			tempList.push(Math.round(data.main.temp))
+			// 			setStore(tempList)
 
-					// icono dinámico
-					console.log(data.weather[0].main)
-					switch (data.weather[0].main) {
-					  case 'Thunderstorm':
-						setOpt({
-						  animationData: thunder,
-						})
-						console.log('TORMENTA');
-						break;
-					  case 'Drizzle':
-						setOpt({
-						  animationData: drizzle,
-						})
-						console.log('LLOVIZNA');
-						break;
-					  case 'Rain':
-						setOpt({
-						  animationData: rain,
-						})
-						console.log('LLUVIA');
-						break;
-					  case 'Snow':
-						setOpt({
-						  animationData: snow,
-						})
-						console.log('NIEVE');
-						break;
-					  case 'Clear':
-						// ASÍ SOLO FUNCIONA CON ALGUNOS SVG
-						// var iconoAnimado = <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64"><g><path fill="none" stroke="#f59e0b" strokeLinecap="round" strokeMiterlimit="10" strokeWidth="3" d="M42.5 32A10.5 10.5 0 1132 21.5 10.5 10.5 0 0142.5 32zM32 15.71V9.5m0 45v-6.21m11.52-27.81l4.39-4.39M16.09 47.91l4.39-4.39m0-23l-4.39-4.39m31.82 31.78l-4.39-4.39M15.71 32H9.5m45 0h-6.21" /><animateTransform attributeName="transform" dur="45s" from="0 32 32" repeatCount="indefinite" to="360 32 32" type="rotate" /></g></svg>
-						// setIconoanimado(iconoAnimado)
-						console.log('LIMPIO');
-						console.log(defaultOptions.animationData)
-						setOpt({
-						  animationData: clear,
-						})
-						break;
-					  case 'Atmosphere':
-						setOpt({
-						  animationData: atmosphere,
-						})
-					   
-						console.log('ATMOSFERA');
-						break;
-					  case 'Clouds':
-						setOpt({
-						  animationData: cloudy,
-						})
-						console.log('NUBES');
-						break;
-					  default:         
-						setOpt({
-						  animationData: notavailable,
-						})
-						console.log('por defecto');
-					}
-			
-				  })
-				  .catch((error) => {
-					//error handling
-					console.error("ERROR:", error);
-				  });
-			},
+			// 			// icono de la API estático
+			// 			// let iconCode = data.weather[0].icon
+			// 			// const urlIcon = `http://openweathermap.org/img/wn/${iconCode}.png`
+			// 			// let { urlIconsList } = getStore()
+			// 			// urlIconsList.push(urlIcon)
+
+			// 		})
+			// 		.catch((error) => {
+			// 			//error handling
+			// 			console.error("ERROR:", error);
+			// 		});
+			// 	})
+
+			// },
 			getMarkers: () => {
 				let { markers } = getStore()
 				const { activities } = getStore()
-				console.log(activities.length, "long activities")
+				// console.log(activities.length, "long activities")
 
 				if (activities.length > markers.length) {
 					activities.map((value, index) => {
@@ -146,7 +97,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 						setStore(markers)
 					})
 				}
-				console.log(markers, "markers del flux")
+				// console.log(markers, "markers del flux")
 			},
 			getActivities: async () => {
 				await fetch(
@@ -165,6 +116,71 @@ const getState = ({ getStore, getActions, setStore }) => {
 							activities: data.result,
 						});
 
+						let { locationList } = getStore()
+
+						let { activities } = getStore()
+						activities.map((value, index) => {
+							console.log(value.city, "city del map")
+							let location = value.city
+							locationList.push(location)
+							setStore(locationList)
+						})
+
+
+						console.log(locationList, "location get weather")
+						locationList.map((value, index) => {
+							const url = "https://api.openweathermap.org/data/2.5/weather?q=" + value + "&lang=en&units=metric&appid=74b3467d2c3033271c21502ee8e7ca5e"
+							console.log(url, "url")
+							fetch(url)
+								.then((resp) => {
+									if (resp.ok) {
+										console.log("El request se hizo bien");
+										return resp.json();
+									} else {
+										console.log("Hubo un Error " + resp.status + " en el request");
+									}
+								})
+								.then((data) => {
+									//here is were your code should start after the fetch finishes
+									console.log("Este es el body del request", data); //this will print on the console the exact object received from the server
+									// console.log(body.map((t) => t.label));
+									// setLista(body.map((t) => t.label));
+									console.log(data.main.temp)
+									// setTemp(Math.round(data.main.temp))
+									let { tempList } = getStore()
+									tempList.push(Math.round(data.main.temp))
+									setStore(tempList)
+
+									let {iconCode} = getStore()
+									let {iconsList} = getStore()
+									let {urlIcon} = getStore()
+
+									// icono de la API estático
+									iconCode = data.weather[0].icon
+									urlIcon = `http://openweathermap.org/img/wn/${iconCode}.png`
+									iconsList.push(urlIcon)
+									setStore(iconsList)
+
+								})
+								.catch((error) => {
+									//error handling
+									console.error("ERROR:", error);
+								});
+						})
+
+
+
+
+
+						// location.push(data.result.city)
+						// console.log(locationList, "array de locations flux")
+						// const { activities } = getStore()
+						// const { location } = getStore()
+						// activities.map((value)=>{
+						// 	location = value.city
+						// 	console.log(value.city, "value.city")
+						// 	getWeather(location,value.date)
+						// })
 
 					})
 
@@ -276,7 +292,7 @@ const getState = ({ getStore, getActions, setStore }) => {
 
 				//if (tok == getStore().token) {
 
-				  await fetch(
+				await fetch(
 
 
 					"https://3001-miguelubeda-teamder-ygfdc0g635s.ws-eu63.gitpod.io/api/privated",
@@ -305,75 +321,75 @@ const getState = ({ getStore, getActions, setStore }) => {
 				//   return "Validation error flux 97";
 				// }
 
-			  },
-			
-			  getActivities: async () => {
-				
-				
-				fetch(
-                    "https://3001-miguelubeda-teamder-ygfdc0g635s.ws-eu63.gitpod.io/api/getAllActivities"
-                  )
-                    .then((resp) => {
-                      if (resp.ok) {
-                        console.log("El request se hizo bien" );
-                        return resp.json();
-                      } else {
-                        console.log("Hubo un Error " + resp.status + " en el request");
-                      }
-                    })
-                    .then((data) => {
-						setStore({activities: data.result})
-                    })
-                    .catch((error) => {
-                      //error handling
-                      console.error("ERROR:", error);
-                    });
 			},
+
+			// getActivities: async () => {
+
+
+			// 	fetch(
+			// 		"https://3001-miguelubeda-teamder-ygfdc0g635s.ws-eu63.gitpod.io/api/getAllActivities"
+			// 	)
+			// 		.then((resp) => {
+			// 			if (resp.ok) {
+			// 				console.log("El request se hizo bien");
+			// 				return resp.json();
+			// 			} else {
+			// 				console.log("Hubo un Error " + resp.status + " en el request");
+			// 			}
+			// 		})
+			// 		.then((data) => {
+			// 			setStore({ activities: data.result })
+			// 		})
+			// 		.catch((error) => {
+			// 			//error handling
+			// 			console.error("ERROR:", error);
+			// 		});
+			// },
 
 			getTargetActivities: async () => {
 				let tok = localStorage.getItem("token");
 				fetch(
-                    "https://3001-miguelubeda-teamder-ygfdc0g635s.ws-eu63.gitpod.io/api/getTargetActivities"
-                  )
-                    .then((resp) => {
-                      if (resp.ok) {
-                        console.log("El request se hizo bien" );
-                        return resp.json();
-                      } else {
-                        console.log("Hubo un Error " + resp.status + " en el request");
-                      }
-                    })
-                    .then((data) => {
-						setStore({userActivities: data.result})
-                    })
-                    .catch((error) => {
-                      //error handling
-                      console.error("ERROR:", error);
-                    });
+					"https://3001-miguelubeda-teamder-ygfdc0g635s.ws-eu63.gitpod.io/api/getTargetActivities"
+				)
+					.then((resp) => {
+						if (resp.ok) {
+							console.log("El request se hizo bien");
+							return resp.json();
+						} else {
+							console.log("Hubo un Error " + resp.status + " en el request");
+						}
+					})
+					.then((data) => {
+						setStore({ userActivities: data.result })
+					})
+					.catch((error) => {
+						//error handling
+						console.error("ERROR:", error);
+					});
 			},
 
 			getPostedActivities: async () => {
 				let tok = localStorage.getItem("token");
 				fetch(
-                    "https://3001-miguelubeda-teamder-ygfdc0g635s.ws-eu63.gitpod.io/api/getPostedActivities",
+					"https://3001-miguelubeda-teamder-ygfdc0g635s.ws-eu63.gitpod.io/api/getPostedActivities",
 					{
 						method: "GET",
 						headers: {
-						  "Content-Type": "application/json",
-						  Authorization: "Bearer " + tok,
+							"Content-Type": "application/json",
+							Authorization: "Bearer " + tok,
 						},
-					  }
-                  )
-                    .then((resp) => {
-                      if (resp.ok) {
-                        console.log("El request se hizo bien" );
-                        return resp.json();
-                      } else {
-                        console.log("Hubo un Error " + resp.status + " en el request");
-                      }
-                    })
-                    .then((data) => {
-						setStore({postedActivities: data.Posted_Activities})
+					}
+				)
+					.then((resp) => {
+						if (resp.ok) {
+							console.log("El request se hizo bien");
+							return resp.json();
+						} else {
+							console.log("Hubo un Error " + resp.status + " en el request");
+						}
+					})
+					.then((data) => {
+						setStore({ postedActivities: data.Posted_Activities })
 						let { dates } = getStore()
 						const { postedActivities } = getStore()
 
@@ -387,11 +403,11 @@ const getState = ({ getStore, getActions, setStore }) => {
 							})
 						}
 						console.log(dates, "dates del flux")
-                    })
-                    .catch((error) => {
-                      //error handling
-                      console.error("ERROR:", error);
-                    });
+					})
+					.catch((error) => {
+						//error handling
+						console.error("ERROR:", error);
+					});
 			},
 
 
