49ca7b0eec950e3955f2bbcdd025fb5f3fc2bc94
siemeris <siemeris@gmail.com>
2022-08-30 12:42:40 +0000

    getWeather en Flux

--- src/front/js/component/cardDetalle.js
+++ src/front/js/component/cardDetalle.js
@@ -5,15 +5,36 @@ import soccer from "/workspace/Teamder/src/front/img/Diseño sin título.png";
 
 export const CardDetalle = () => {
   const { store, actions } = useContext(Context);
+  const [temp, setTemp] = useState("");
+  const [icon, setIcon] = useState("");
+  const [defaultOptions, setOpt] = useState({
+    loop: true,
+    autoplay: true,
+    animationData: null,
+    rendererSettings: {
+      preserveAspectRatio: "xMidYMid slice"
+    }
+  })
+  const [location, setLocation] = useState("")
 
-  useEffect(() => {
-      actions.getActivities();
-    }, []);
+ 
+  // const url2 = "https://api.openweathermap.org/data/2.5/forecast?q=" + location + "&appid=74b3467d2c3033271c21502ee8e7ca5e&units=metric"
+
+  useEffect(()=>{actions.getActivities();},[])
+  
+useEffect(()=>{
+   actions.getWeather()
+ },[store.location])
+  
 
   return (
     <div className="container mt-4">
-      <div className="row d-flex flex-column" style={{height: "15rem", overflowX:"auto"}}>
+      <div className="row d-flex flex-column" style={{height: "16rem", overflowX:"auto"}}>
       {store.activities.map((value,index) => {
+        store.location=value.city
+        console.log(store.location, "store.location")
+        console.log(value.city)
+        // actions.getWeather()
         return (
           
           <div key={index} className="card px-1 mx-2 py-1 border border-light shadow border-4 rounded-1" style={{height: "14rem", width: '14rem'}}>
@@ -32,6 +53,11 @@ export const CardDetalle = () => {
               </h5>
               <h6 className="card-subtitle mb-2 text-muted ">{value.date}</h6>
               <h6 className="card-subtitle mb-2 text-muted">{value.location}, {value.city}</h6>
+                  <span>
+                    
+                    <img src={store.urlIcons[index]}></img>
+                    {temp}ºC
+                  </span>
               <hr></hr>
               <div className="text-center mt-2">
                 <button
--- src/front/js/store/flux.js
+++ src/front/js/store/flux.js
@@ -22,10 +22,102 @@ const getState = ({ getStore, getActions, setStore }) => {
 			activities: [],
 			markers: [],
 			dates: [],
-
+			urlIcons:[],
+			location: "",
 
 		},
 		actions: {
+			getWeather: ()=> {
+				let { location } = getStore()
+				console.log(location, "location")
+				const url = `https://api.openweathermap.org/data/2.5/weather?q=` + location + `&lang=en&units=metric&appid=74b3467d2c3033271c21502ee8e7ca5e`
+				  fetch(url)
+				  .then((resp) => {
+					if (resp.ok) {
+					  console.log("El request se hizo bien");
+					  return resp.json();
+					} else {
+					  console.log("Hubo un Error " + resp.status + " en el request");
+					}
+				  })
+				  .then((data) => {
+					//here is were your code should start after the fetch finishes
+					console.log("Este es el body del request", data); //this will print on the console the exact object received from the server
+					// console.log(body.map((t) => t.label));
+					// setLista(body.map((t) => t.label));
+					console.log(data.main.temp)
+					setTemp(Math.round(data.main.temp))
+			
+					// icono de la API estático
+					let iconCode = data.weather[0].icon
+					const urlIcon = `http://openweathermap.org/img/wn/${iconCode}.png`
+					let { urlIcons } = getStore()
+					urlIcons.push(urlIcon)
+					
+
+					// icono dinámico
+					console.log(data.weather[0].main)
+					switch (data.weather[0].main) {
+					  case 'Thunderstorm':
+						setOpt({
+						  animationData: thunder,
+						})
+						console.log('TORMENTA');
+						break;
+					  case 'Drizzle':
+						setOpt({
+						  animationData: drizzle,
+						})
+						console.log('LLOVIZNA');
+						break;
+					  case 'Rain':
+						setOpt({
+						  animationData: rain,
+						})
+						console.log('LLUVIA');
+						break;
+					  case 'Snow':
+						setOpt({
+						  animationData: snow,
+						})
+						console.log('NIEVE');
+						break;
+					  case 'Clear':
+						// ASÍ SOLO FUNCIONA CON ALGUNOS SVG
+						// var iconoAnimado = <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 64 64"><g><path fill="none" stroke="#f59e0b" strokeLinecap="round" strokeMiterlimit="10" strokeWidth="3" d="M42.5 32A10.5 10.5 0 1132 21.5 10.5 10.5 0 0142.5 32zM32 15.71V9.5m0 45v-6.21m11.52-27.81l4.39-4.39M16.09 47.91l4.39-4.39m0-23l-4.39-4.39m31.82 31.78l-4.39-4.39M15.71 32H9.5m45 0h-6.21" /><animateTransform attributeName="transform" dur="45s" from="0 32 32" repeatCount="indefinite" to="360 32 32" type="rotate" /></g></svg>
+						// setIconoanimado(iconoAnimado)
+						console.log('LIMPIO');
+						console.log(defaultOptions.animationData)
+						setOpt({
+						  animationData: clear,
+						})
+						break;
+					  case 'Atmosphere':
+						setOpt({
+						  animationData: atmosphere,
+						})
+					   
+						console.log('ATMOSFERA');
+						break;
+					  case 'Clouds':
+						setOpt({
+						  animationData: cloudy,
+						})
+						console.log('NUBES');
+						break;
+					  default:         
+						setOpt({
+						  animationData: notavailable,
+						})
+						console.log('por defecto');
+					}
+			
+				  })
+				  .catch((error) => {
+					//error handling
+					console.error("ERROR:", error);
+				  });
+			},
 			getMarkers: () => {
 				let { markers } = getStore()
 				const { activities } = getStore()
